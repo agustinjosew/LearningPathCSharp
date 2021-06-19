574765c3b6fa49a2075d82d7f808267cdbbd9130
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-06-19 20:53:07 -0300

    flow control sin TDD

---
+++ 2-Flow Control, Arrays, and Exception Handling/EssentialTrainning/EssentialTraining/FlowControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTraining
+{
+    public class FlowControl
+    {
+        public bool MiColorPreferidoEsAzul(string color)
+        {
+            if (color.ToLower() == "Azul")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+}
