1048942b1d93770626a4cff4ea5a95f1b62c57d3
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-06-19 19:45:49 -0300

    List <T> sin TDD

---
+++ 2-Flow Control, Arrays, and Exception Handling/EssentialTrainning/EssentialTraining/SalsaSuper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTraining
+{
+    public class SalsaSuper
+    {
+        public List<string> Salsas { get; set; }
+
+        //constructor
+        public SalsaSuper()
+        {
+            Salsas = new List<string>();
+        }
+        public bool LaSalsaExiste (string salsa)
+        {
+            return Salsas.Contains(salsa);
+        }
+
+
+    }
+}
