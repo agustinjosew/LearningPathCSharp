2036632b6e2de218fab18a50179a84e99392b0a9
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-06-19 19:58:41 -0300

    List <T> TDD

---
+++ 2-Flow Control, Arrays, and Exception Handling/EssentialTrainning/EssentialTrainingTest/SalsaSuperTest.cs
@@ -0,0 +1,24 @@
+﻿using EssentialTraining;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EssentialTrainingTest
+{[TestClass]
+    public class SalsaSuperTest
+    {
+        [TestMethod]
+        public void LaSalsaExisteTest()
+        {
+            var testInstancia = new SalsaSuper();
+            testInstancia.Salsas.Add("Tabasco");
+            testInstancia.Salsas.Add("Cholula");
+            testInstancia.Salsas.Add("Chespirito");
+
+            //espero TRUE
+            Assert.IsTrue(testInstancia.LaSalsaExiste("Cholula"));
+
+            //espero FALSE
+            Assert.IsFalse(testInstancia.LaSalsaExiste("Gordillo"));
+
+        }
+    }
+}
--- 2-Flow Control, Arrays, and Exception Handling/EssentialTrainning/EssentialTrainingTest/UnitTest1.cs
+++ 2-Flow Control, Arrays, and Exception Handling/EssentialTrainning/EssentialTrainingTest/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace EssentialTrainingTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMetodo1()
+        public void TestMetodo1TRUE()
         {
             var testInstancia = new Class1();
             var testResultado = testInstancia.SumarDosNumeros(10, 5);
@@ -16,11 +16,11 @@ namespace EssentialTrainingTest
         }
 
         [TestMethod]
-        public void TestMetodo2()
+        public void TestMetodo2FALSE()
         {
             var testInstancia = new Class1();
             var testResultado = testInstancia.SumarDosNumeros(10, 5);
-            Assert.AreEqual(14, testResultado, "Espero 10 + 5 igual 15");
+            Assert.AreNotEqual(14, testResultado, "Espero 10 + 5 igual 15");
         }
     }
 }
